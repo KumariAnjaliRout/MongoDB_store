bda73274cea3ec1b16350ab5b24908a219b39bea
KumariAnjaliRout <151063318+KumariAnjaliRout@users.noreply.github.com>
2025-08-15 13:27:28 +0530

    updation

--- client/src/components/Record.jsx
+++ client/src/components/Record.jsx
@@ -17,7 +17,7 @@ export default function Record() {
       if(!id) return;
       setIsNew(false);
       const response = await fetch(
-        `http://localhost:5050/record/${params.id.toString()}`
+        ` https://mongodb-store-0zkz.onrender.com${params.id.toString()}`
       );
       if (!response.ok) {
         const message = `An error has occurred: ${response.statusText}`;
@@ -51,7 +51,7 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch("http://localhost:5050/record", {
+        response = await fetch(" https://mongodb-store-0zkz.onrender.com", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -59,8 +59,8 @@ export default function Record() {
           body: JSON.stringify(person),
         });
       } else {
-        // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://localhost:5050/record/${params.id}`, {
+        // if we are updating a record we willh PATCH to /record/:id.
+        response = await fetch(`https://mongodb-store-0zkz.onrender.com${params.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
